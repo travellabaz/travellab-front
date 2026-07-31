20ab9e309637b3072f190069228583b5777f72a4
Elnur Shirmamammadov <eshirmammadov@avo.uz>
2026-07-31 12:15:42 +0400

    Replace guessed Partners logos with exact Figma vector export

--- src/sections/PartnersSection.jsx
+++ src/sections/PartnersSection.jsx
@@ -1,3 +1,5 @@
+import PartnersLogos from './PartnersLogos';
+
 export default function PartnersSection() {
   return (
     <section>
@@ -8,21 +10,8 @@ export default function PartnersSection() {
             <h2 className="tl-title">Etibarlı Tərəfdaşlarımız</h2>
           </div>
         </div>
-        <div className="tl-partners">
-          <div className="tl-partner" style={{ color: '#333333' }}>Hotels.com</div>
-          <div className="tl-partner" style={{ color: '#5A5A5A' }}>agoda</div>
-          <div className="tl-partner" style={{ color: '#1550A4', letterSpacing: 1 }}>AMADEUS</div>
-          <div className="tl-partner">
-            <div className="tl-partner-icon" style={{ color: '#FF385C' }}>
-              <svg width="18" height="18" viewBox="0 0 32 32" fill="#FF385C"><path d="M16 2C12 9 6 16.5 6 22a10 10 0 0020 0c0-5.5-6-13-10-20z" /></svg>
-              airbnb
-            </div>
-          </div>
-          <div className="tl-partner">
-            <div className="tl-partner-icon" style={{ color: 'var(--tl-navy)' }}>
-              make<span style={{ background: '#EB2F96', color: '#fff', borderRadius: 5, padding: '1px 7px', fontStyle: 'italic' }}>my</span>trip
-            </div>
-          </div>
+        <div className="tl-partners-row">
+          <PartnersLogos />
         </div>
       </div>
     </section>
